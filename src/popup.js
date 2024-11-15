a4eb56a8ebd062cb70ac729dda04ba8d444ec9c6
Ibrahim2307 <aibrahim.0505@gmail.com>
2024-11-15 22:26:26 +0400

    project1

---
+++ src/popup.js
@@ -0,0 +1,7 @@
+const dataList = document.getElementById('data-list');
+const addFieldBtn = document.getElementById('add-field-btn');
+const dataForm = document.getElementById('data-form');
+const fieldKeyInput = document.getElementById('field-key');
+const fieldValueInput = document.getElementById('field-value');
+const cancelBtn = document.getElementById('cancel-btn');
+
