04c9b2651b068fd67a44ce194ff92108ce581e69
Ibrahim2307 <aibrahim.0505@gmail.com>
2024-11-20 23:42:55 +0400

    Project 1

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,5 @@
-const dataList = document.getElementById('data-list');
-const addFieldBtn = document.getElementById('add-field-btn');
-const dataForm = document.getElementById('data-form');
-const fieldKeyInput = document.getElementById('field-key');
-const fieldValueInput = document.getElementById('field-value');
-const cancelBtn = document.getElementById('cancel-btn');
-
+const form = document.getElementById("custom-field-form");
+const fieldNameInput = document.getElementById("field-name");
+const fieldValueInput = document.getElementById("field-value");
+const fieldList = document.getElementById("field-list");
+const fetchLinkedInDataBtn = document.getElementById("fetch-linkedin-data");
