d11def31e915226d872d94883de5487fb8d87bba
Farid Mammadli <faridapril1982@gmail.com>
2024-11-29 19:27:51 +0400

    Project 1

--- src/background.js
+++ src/background.js
@@ -1,14 +1,6 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "extractLinkedInData") {
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: request.tab.id },
-        files: ["./linkedinExtractor.js"],
-      },
-      () => {
-        sendResponse({ success: true });
-      }
-    );
-    return true;
-  }
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content.js']
+  });
 });
